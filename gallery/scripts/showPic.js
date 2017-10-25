b3fd5c77c7b9e9c3c8d6fef8dea2a39b5e2bcb04
zzy28 <zzy1104@hotmail.com>
2017-10-25 11:18:14 +0800

    dongtaichuangjian

--- gallery/scripts/showPic.js
+++ gallery/scripts/showPic.js
@@ -41,6 +41,34 @@ function addLoadEvent(func){
 	}
 }
 
+function insertAfter(newElement,targetElement){
+	var parent = targetElement.parentNode;
+	if (parent.lastChild == targetElement){
+		parent.appendChild(newElement);
+	} else {
+	parent.insertBefore(newElement,targetElement.nextSibling);
+	}
+}
+
+function preparePlaceholder(){
+	if (!document.createElement) return false;
+	if (!document.createTextNode) return false;
+	if (!document.getElementById) return false;
+	if (!document.getElementById("imagegallery")) return false;
+	var placeholder = document.createElement("img");
+	placeholder.setAttribute("id","placeholder");
+	placeholder.setAttribute("src","images/placeholder.jpg");
+	placeholder.setAttribute("alt","my image gallery");
+	var description = document.createElement("p");
+	description.setAttribute("id","description");
+	var desctext = document.createTextNode("Choose an image");
+	description.appendChild(desctext);
+	var gallery = document.getElementById("imagegallery");
+	insertAfter("placeholder,gallery");
+	insertAfter("description,placeholder");
+}
+
 addLoadEvent(prepareGallery);
+addLoadEvent(preparePlaceholder);
 
 
